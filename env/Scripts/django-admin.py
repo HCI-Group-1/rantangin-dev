ac4f0ac83ca9fee64a9970180e4bca5f59fa1d36
Ahmad Luhur Pakerti <ahmadluhurp@gmail.com>
2021-11-15 06:09:40 +0700

    Remove a hardcoded Django secret key

--- env/Scripts/django-admin.py
+++ env/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\adike\Documents\GitHub\rantangin-dev\rantangin\env\Scripts\python.exe
+#!C:\Users\adike\Documents\GitHub\rantangin-dev\env\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
